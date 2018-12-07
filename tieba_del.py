543d8a5d0d6aae2ee7c7dcb5e92d3936714cf59f
zhoujian <zhoujian@cloudatum.com>
2018-12-07 10:03:52 +0800

    tieba_del: delete 30 reply once

--- tieba_del.py
+++ tieba_del.py
@@ -5,17 +5,21 @@ from baidu import extract_post_path, del_post
 
 
 if __name__ == '__main__':
-    post_path = extract_post_path()
-    _l = len(post_path)
     print('delete begin', end=' ', flush=True)
-    for i, post_redirect_path in enumerate(post_path, start=1):
-        r = del_post(post_redirect_path)
-        if r['no'] != 0:
-            print()
-            print(r)
-            break
-        print(' . ', end=' ', flush=True)
-        if i != _l:
-            time.sleep(0.5)
-        else:
-            print('end')
+    count = 0
+    while count <= 30:
+        post_path = extract_post_path()
+        _l = len(post_path)
+        for i, post_redirect_path in enumerate(post_path, start=1):
+            r = del_post(post_redirect_path)
+            if r['no'] != 0:
+                count = 31
+                print()
+                print(r)
+                break
+            print(' . ', end=' ', flush=True)
+            count += 1
+            if i != _l:
+                time.sleep(0.5)
+    print('end')
+    print('%s replies deleted')
