a276eac16d4d48aaab03873c025fbfe25ac5bdde
zhoujian <zhoujian@cloudatum.com>
2018-12-25 14:26:59 +0800

    bugFix: add request timeout to requests param

--- html_parser/__init__.py
+++ html_parser/__init__.py
@@ -3,7 +3,7 @@ from lxml import etree
 
 
 def extract_content(url, xpath, headers=None):
-    r = requests.get(url, headers=headers)
+    r = requests.get(url, headers=headers, timeout=5)
     selector = etree.HTML(r.text)
     content = selector.xpath(xpath)
     return content
