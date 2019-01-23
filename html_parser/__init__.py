d19c6b9dd23216d88e75945afc1234f91b68296d
zhoujian <zhoujian@cloudatum.com>
2019-01-23 14:45:53 +0800

    bugFix: add timeout;add result cache

--- html_parser/__init__.py
+++ html_parser/__init__.py
@@ -3,7 +3,7 @@ from lxml import etree
 
 
 def extract_content(url, xpath, headers=None):
-    r = requests.get(url, headers=headers, timeout=5)
+    r = requests.get(url, headers=headers, timeout=10)
     selector = etree.HTML(r.text)
     content = selector.xpath(xpath)
     return content
--- server/flask_server.py
+++ server/flask_server.py
@@ -1,17 +1,36 @@
 from flask import Flask, render_template
 from gumball import generate_list
 import json
+import time
 
 app = Flask(__name__)
+
+cacheData = {}
 
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    _data_list = []
-    for _url, _name, _score, _contains in generate_list():
-        _data_list.append({'url': _url, 'name': _name, 'score': _score, 'contains': _contains})
-    _s = json.dumps(_data_list, ensure_ascii=False)
-    return render_template('./home.html', data=_s)
+    now = int(time.time())
+    timestamp = cacheData.get('timestamp', None)
+    expired = False
+    s = ''
+    if not timestamp or now - timestamp > 3600:
+        expired = True
+    else:
+        cached = cacheData.get('data', None)
+        if cached:
+            s = json.dumps(cached, ensure_ascii=False)
+        else:
+            expired = True
+    if expired:
+        data_list = []
+        for url, name, score, contains in generate_list():
+            data_list.append({'url': url, 'name': name, 'score': score, 'contains': contains})
+        cacheData['data'] = data_list
+        cacheData['timestamp'] = now
+        s = json.dumps(data_list, ensure_ascii=False)
+
+    return render_template('./home.html', data=s)
 
 
 def run():
