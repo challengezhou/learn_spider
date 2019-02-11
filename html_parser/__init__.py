eacf035ad92af5da68845e3e3ca0d239b25d8798
zhoujian <zhoujian@cloudatum.com>
2019-02-11 17:21:03 +0800

    bugFix: add error catch

--- html_parser/__init__.py
+++ html_parser/__init__.py
@@ -3,7 +3,11 @@ from lxml import etree
 
 
 def extract_content(url, xpath, headers=None):
-    r = requests.get(url, headers=headers, timeout=10)
+    try:
+        r = requests.get(url, headers=headers, timeout=10)
+    except requests.exceptions.ConnectionError as error:
+        print('[ERROR] Call |%s| error msg:%s' % (url, error))
+        return None
     selector = etree.HTML(r.text)
     content = selector.xpath(xpath)
     return content
